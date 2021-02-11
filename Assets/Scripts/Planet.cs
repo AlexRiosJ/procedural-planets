ee049da107a7fdae8c4b89d9891dd096222775ab
Alejandro Ríos Jasso <jassoalex97@hotmail.com>
2021-02-10 19:47:26 -0600

    Changing to 4 Spaces for Identation

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -4,52 +4,52 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-  [Range(2, 256)]
-  public int resolution = 10;
+    [Range(2, 256)]
+    public int resolution = 10;
 
-  [SerializeField, HideInInspector]
-  MeshFilter[] meshFilters;
-  TerrainFace[] terrainFaces;
+    [SerializeField, HideInInspector]
+    MeshFilter[] meshFilters;
+    TerrainFace[] terrainFaces;
 
-  void OnValidate()
-  {
-    Initialize();
-    GenerateMesh();
-  }
-
-  void Initialize()
-  {
-    if (meshFilters == null || meshFilters.Length == 0)
+    void OnValidate()
     {
-      meshFilters = new MeshFilter[6];
+        Initialize();
+        GenerateMesh();
     }
-    terrainFaces = new TerrainFace[6];
 
-    Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
-
-    for (int i = 0; i < 6; i++)
+    void Initialize()
     {
-      if (meshFilters[i] == null)
-      {
-        GameObject meshObj = new GameObject("mesh");
-        meshObj.transform.parent = transform;
+        if (meshFilters == null || meshFilters.Length == 0)
+        {
+            meshFilters = new MeshFilter[6];
+        }
+        terrainFaces = new TerrainFace[6];
 
-        meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
-        meshFilters[i] = meshObj.AddComponent<MeshFilter>();
-        meshFilters[i].sharedMesh = new Mesh();
-      }
+        Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
 
-      terrainFaces[i] = new TerrainFace(meshFilters[i].sharedMesh, resolution, directions[i]);
+        for (int i = 0; i < 6; i++)
+        {
+            if (meshFilters[i] == null)
+            {
+                GameObject meshObj = new GameObject("mesh");
+                meshObj.transform.parent = transform;
 
+                meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+                meshFilters[i] = meshObj.AddComponent<MeshFilter>();
+                meshFilters[i].sharedMesh = new Mesh();
+            }
+
+            terrainFaces[i] = new TerrainFace(meshFilters[i].sharedMesh, resolution, directions[i]);
+
+        }
     }
-  }
 
-  void GenerateMesh()
-  {
-    foreach (TerrainFace face in terrainFaces)
+    void GenerateMesh()
     {
-      face.ConstructMesh();
+        foreach (TerrainFace face in terrainFaces)
+        {
+            face.ConstructMesh();
+        }
     }
-  }
 
 }
